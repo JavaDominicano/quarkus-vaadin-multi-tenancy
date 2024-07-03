4c0e8235bdc02b68d2877c49b1eb11bf69159a1f
Juan Diego López V <juandiegolopezve@gmail.com>
2024-07-02 20:26:46 -0400

    remove unsed files of vaadin

--- frontend/routes.tsx
+++
@@ -1,8 +0,0 @@
-import { createBrowserRouter, RouteObject } from 'react-router-dom';
-import { serverSideRoutes } from "Frontend/generated/flow/Flow";
-
-export const routes = [
-  ...serverSideRoutes
-] as RouteObject[];
-
-export default createBrowserRouter(routes);
