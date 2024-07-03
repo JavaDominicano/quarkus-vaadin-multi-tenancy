5126a3bf10d23c0611571317afc1cf25daa6d74a
Juan Diego López V <juandiegolopezve@gmail.com>
2024-07-03 09:29:53 -0400

    refactor

--- src/main/java/org/jconfdominicana/security/CustomAuthMechanism.java
+++ src/main/java/org/jconfdominicana/security/CustomAuthMechanism.java
@@ -1,20 +1,18 @@
 package org.jconfdominicana.security;
 
 import io.netty.handler.codec.http.HttpHeaderNames;
-import io.netty.handler.codec.http.HttpHeaders;
 import io.netty.handler.codec.http.HttpResponseStatus;
-import io.quarkus.security.credential.PasswordCredential;
 import io.quarkus.security.identity.IdentityProviderManager;
 import io.quarkus.security.identity.SecurityIdentity;
 import io.quarkus.security.identity.request.AuthenticationRequest;
 import io.quarkus.security.identity.request.TokenAuthenticationRequest;
-import io.quarkus.security.identity.request.TrustedAuthenticationRequest;
-import io.quarkus.security.identity.request.UsernamePasswordAuthenticationRequest;
-import io.quarkus.vertx.http.runtime.security.*;
+import io.quarkus.vertx.http.runtime.security.ChallengeData;
+import io.quarkus.vertx.http.runtime.security.HttpAuthenticationMechanism;
+import io.quarkus.vertx.http.runtime.security.HttpCredentialTransport;
+import io.quarkus.vertx.http.runtime.security.HttpSecurityUtils;
 import io.smallrye.mutiny.Uni;
 import io.vertx.ext.web.RoutingContext;
 import jakarta.enterprise.context.ApplicationScoped;
-import lombok.AllArgsConstructor;
 
 import java.util.Collections;
 import java.util.Set;
--- src/main/java/org/jconfdominicana/security/SecurityService.java
+++ src/main/java/org/jconfdominicana/security/SecurityService.java
@@ -4,10 +4,8 @@ import com.vaadin.flow.component.UI;
 import com.vaadin.flow.server.VaadinResponse;
 import io.quarkus.security.runtime.SecurityIdentityAssociation;
 import jakarta.enterprise.context.RequestScoped;
-import jakarta.servlet.http.Cookie;
-import jakarta.ws.rs.CookieParam;
+import jakarta.ws.rs.core.Cookie;
 import jakarta.ws.rs.core.NewCookie;
-import jakarta.ws.rs.ext.RuntimeDelegate;
 import lombok.AllArgsConstructor;
 
 @RequestScoped
@@ -22,12 +20,12 @@ public class SecurityService {
 
     public void logout() {
         UI.getCurrent().getPage().setLocation("/login");
-        NewCookie cookie = new NewCookie.Builder("quarkus-credential")
+        Cookie cookie = new NewCookie.Builder("quarkus-credential")
                 .path("/")
                 .maxAge(0)
                 .build();
 
-        VaadinResponse.getCurrent().addCookie(new Cookie("quarkus-credential", cookie.getValue()));
+        VaadinResponse.getCurrent().addCookie(new jakarta.servlet.http.Cookie("quarkus-credential", cookie.getValue()));
     }
 
 }
